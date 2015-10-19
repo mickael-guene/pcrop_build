9e49dd97ddb79011705359fed022a0d830d3b6a9
Mickael Guene <mickael.guene@st.com>
2015-10-19 10:59:43 +0200

    Use python26 if it exist

--- aci/delivery.sh
+++ aci/delivery.sh
@@ -6,12 +6,19 @@ SCRIPTDIR=`(cd $SCRIPTDIR ; pwd)`
 #get working directory
 TOP=`pwd`
 
+#select python version
+if [ `which python26` ]; then
+    PYTHON=`which python26`
+else
+    PYTHON=`which python`
+fi
+
 # define version
 cd ${TOP}/.repo/manifests
 TAG_NAME=`git describe --tags --always 2>/dev/null`
 cd ${TOP}
 # push packages
 for f in ${TOP}/out/* ; do
-    python ${TOP}/scratch/build/aci/upload_release.py -u mickael-guene -r pcrop_manifest -t $TAG_NAME $f
+    ${PYTHON} ${TOP}/scratch/build/aci/upload_release.py -u mickael-guene -r pcrop_manifest -t $TAG_NAME $f
 done
 
